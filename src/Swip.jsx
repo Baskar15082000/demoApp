f1c74fd279b1535eb8f3ab35cfab675118efed8b
Baskar R <134188943+Baskar15082000@users.noreply.github.com>
2024-02-24 11:50:31 +0530

    Update Swip.jsx

--- src/Swip.jsx
+++ src/Swip.jsx
@@ -13,7 +13,7 @@ const Swip = () => {
     setSlide(currentSlide);
   };
   const arr=["https://static.vecteezy.com/system/resources/thumbnails/028/168/654/small_2x/3d-rendering-of-eu-money-coin-with-sign-of-euro-currency-shattered-into-pieces-falling-apart-depreciation-of-money-financial-crisis-realistic-illustration-isolated-on-transparent-background-png.png","https://cdn-icons-png.flaticon.com/512/8318/8318312.png","https://openclipart.org/image/800px/320548","https://flagsweb.com/images/GIF/Flag_of_India.gif"]
-  const arr2=["CHINA'S ECONOMIC CRISI : ","CURRENT SITUATION","ON THE INTERNATIONAL FRONT","TO INDIA'S ADVANTAGE ?"]
+  const arr2=["CHINA'S ECONOMIC CRISI : ","CURRENT SITUATION","ON THE INTERNATIONAL FRONT","TO INDIA'S ADVANTAGE "]
   const arr3=[" Assessing the gravity os issues behind the stumbling of the Chinese economy", "The continuous decline in chinese stocks has become reason for concern for many across the globe, as it wipe out huge sums of inv3stment dollars.","Us-China relations:Sanctions and trade-related tensions between china and other majorbeconomies like the US add to China's woes.","As per reports, china decline comes at a time when the world is bullish on india."]
   return (
     <div
